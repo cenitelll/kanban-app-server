48dc6e1d68f0bec61233e9a9e13cda59079ca0d5
Theconnoiser <cenitelll@gmail.com>
2023-10-31 20:35:40 +0200

    test vercel2

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
@@ -15,4 +17,18 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/v1", require("./src/v1/routes"));
 
-module.exports = app;
+async function start() {
+    console.log(process.env.MONGODB_URL);
+    try {
+        await mongoose.connect(process.env.MONGODB_URL, {});
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server started on port ${process.env.PORT}...`);
+        });
+    } catch (e) {
+        console.log(e);
+        process.exit(1);
+    }
+}
+
+start();
